def88e20fc7da3984ea9ac75afec7b3d7a388f6a
AmeyaMahajan003 <neelasmahajan003@gmail.com>
2024-05-31 11:10:10 +0530

    Controllers were created for each router

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,27 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const {getContacts,createContact,getContact,updateContact,deleteContact} = require("../controllers/contactController");
 
-router.route("/").get((req,res)=>{
-    // res.send("Server is started");
-    // res.json({message : "Get all contacts"});
-    res.status(200).json({message : "Get all contacts"});
-});
+router.route("/").get(getContacts);
 
-router.route("/").post((req,res)=>{
-    res.status(200).json({message : "Create contacts"});
-});
+router.route("/").post(createContact);
 
-router.route("/:id").get((req,res)=>{
-    res.status(200).json({message : `Get contact for ${req.params.id}`});
-});
+router.route("/:id").get(getContact);
 
-router.route("/:id").put((req,res)=>{
-    res.status(200).json({message : `Update contacts for ${req.params.id}`});
-});
+router.route("/:id").put(updateContact);
 
-router.route("/:id").delete((req,res)=>{
-    res.status(200).json({message : `Delete contacts for ${req.params.id}`});
-});
+router.route("/:id").delete(deleteContact);
 
 
 
