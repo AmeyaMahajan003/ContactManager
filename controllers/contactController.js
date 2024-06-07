f1b2b9030b70578ca9927ff3472c23f05e80f6f4
AmeyaMahajan003 <neelasmahajan003@gmail.com>
2024-06-07 20:48:17 +0530

    Database connection is created and database is running on localhost port no. 27017

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,45 +1,55 @@
+const asyncHandler = require("express-async-handler");
+
+
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access Public
 
-const getContacts = (req,res)=>{
+const getContacts = asyncHandler (async (req,res)=>{
     // res.send("Server is started");
     // res.json({message : "Get all contacts"});
     res.status(200).json({message : "Get all contacts"});
-};
+});
 
 //@desc Create a contacts
 //@route POST /api/contacts
 //@access Public
 
-const createContact = (req,res)=>{
+const createContact = asyncHandler(async (req,res)=>{
     console.log(req.body);
+    const {firstName,lastName,email,phone} = req.body;
+    if(!firstName || !lastName || !email || !phone)
+        {
+            res.status(400);
+            throw new Error("All Fields are mandetory");
+        }
     res.status(200).json({message : "Create contacts"});
-};
+});
 
 //@desc Get contact
 //@route GET /api/contacts/:id
 //@access Public
 
-const getContact = (req,res)=>{
+const getContact = asyncHandler(async (req,res)=>{
     res.status(200).json({message : `Get contact for ${req.params.id}`});
-};
+});
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access Public
 
-const updateContact = (req,res)=>{
+const updateContact = asyncHandler(async (req,res)=>{
     res.status(200).json({message : `Update contacts for ${req.params.id}`});
-};
+});
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access Public
 
-const deleteContact = (req,res)=>{
+const deleteContact = asyncHandler(async (req,res)=>{
     res.status(200).json({message : `Delete contacts for ${req.params.id}`});
-};
+});
 
 
 
---
+++ middleware/errorHandler.js
@@ -0,0 +1,43 @@
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+
+  switch (statusCode) {
+    case 400:
+      res.json({
+        title: "Validation Failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case 404:
+      res.json({
+        title: "NOT FOUND",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case 401:
+      res.json({
+        title: "UNAUTHORIZED",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case 403:
+      res.json({
+        title: "FORBIDDEN",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+      default:
+        console.log("NO ERROR");
+          break;
+
+
+  }
+};
+
+module.exports = errorHandler;
